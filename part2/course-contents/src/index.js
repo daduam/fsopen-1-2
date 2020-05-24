9dd6e1f7533756c076b935b91ca2c66ba5e8c69d
Kwadjo Ampadu <josephampadu549@gmail.com>
2020-05-24 00:19:47 +0000

    do step7 of course contents

--- part2/course-contents/src/index.js
+++ part2/course-contents/src/index.js
@@ -8,7 +8,9 @@ const Header = ({ course }) => {
 }
 
 const Total = ({ course }) => {
-  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+  const sum = course.parts.reduce((total, part) => {
+    return total + part.exercises
+  }, 0)
   return (
     <p>Number of exercises {sum}</p>
   )
@@ -35,7 +37,7 @@ const Content = ({ course }) => {
 const Course = ({ course }) => (
   <div>
     <Header course={course} />
-    {/* <Total course={course} /> */}
+    <Total course={course} />
     <Content course={course} />
   </div>
 )
