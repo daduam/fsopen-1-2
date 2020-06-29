28d679c2e0d97218eee4de777f25af9efc7677b2
Kwadjo Ampadu <josephampadu549@gmail.com>
2020-06-29 21:24:29 +0000

    do step3 of countries-data

--- part2/countries-data/src/components/Country.js
+++ part2/countries-data/src/components/Country.js
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
+
+const Weather = ({ weather, city }) => {
+  if (!weather) {
+    return null;
+  }
+
+  return (
+    <div>
+      <h2>Weather in {city}</h2>
+      <div>
+        <strong>temperature:</strong> {weather.temperature} Celcius
+      </div>
+      <img src={weather.weather_icons[0]} alt={weather.weather_descriptions[0]} />
+      <div>
+        <strong>wind:</strong> {weather.wind_speed} mph direction {weather.wind_dir}
+      </div>
+
+    </div>
+  )
+
+}
 
 const Country = ({ country }) => {
+  const [weather, setWeather] = useState(null)
+
+  const params = {
+    access_key: process.env.REACT_APP_WEATHERSTACK_KEY,
+    query: country.capital
+  }
+
+  const hook = () => {
+    axios
+      .get('http://api.weatherstack.com/current', {params})
+      .then(response => {
+        setWeather(response.data.current)
+      })
+  }
+  // console.log(process.env.REACT_APP_WEATHERSTACK_KEY)
+  useEffect(hook, [])
+
   return (
     <div>
       <h1>{country.name}</h1>
@@ -20,6 +59,7 @@ const Country = ({ country }) => {
       <div>
         <img alt={`flag of ${country.name}`} src={country.flag} width="100px" />
       </div>
+      <Weather weather={weather} city={country.capital} />
     </div>
   )
 }
--- part2/countries-data/src/components/CountryList.js
+++ part2/countries-data/src/components/CountryList.js
@@ -3,17 +3,19 @@ import Country from './Country'
 
 
 const CountryList = ({ countries, onClick }) => {
-  if (countries.length > 10) {
+  if (countries.length === 0) {
     return (
-      <div>Too many matches, specify another filter</div>
+      <div>
+        no matches
+      </div>
     )
   }
-  else if (countries.length === 1) {
+  if (countries.length === 1) {
     return (
       <Country country={countries[0]} />
     )
   }
-  else {
+  if (countries.length < 10) {
     return (
       <div>
         {countries.map((country) =>
@@ -25,6 +27,10 @@ const CountryList = ({ countries, onClick }) => {
       </div>
     )
   }
+
+  return (
+    <div>Too many matches, specify another filter</div>
+  )
 }
 
 export default CountryList
